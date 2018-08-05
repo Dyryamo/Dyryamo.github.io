f466ed664bc78c6f6273bfc2508fbd62bc57bff5
zhong <519233982@qq.com>
2018-08-05 15:26:11 +0800

    Site updated: 2018-08-05 15:26:10

---
+++ tags/review/GCD.cpp
@@ -0,0 +1,25 @@
+/*    The greatest common divisor GCD(a,b) of two positive integers a and b,sometimes written (a,b),is the largest divisor common to a and b,For example,(1,2)=1,(12,18)=6.
+    (a,b) can be easily found by the Euclidean algorithm. Now Carp is considering a little more difficult problem:
+    Given integers N and M,please answer sum of  X satisfies 1<=X<=N and (X,N)>=M.
+
+输入
+    The first line of input is an integer T(T<=100) representing the number of test cases. The following T lines each contains two numbers N and M (1<=N<=10^9, 1<=M<=10^9), representing a test case.
+输出
+    Output the answer mod 1000000007
+样例输入
+
+    3
+    1 1
+    10 2
+    10000 72
+
+*/
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+int main(){
+
+    return ;
+}
